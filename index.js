08d8ea2bbf566640d3f8115f6d64ca8fe9a9a14b
daspete <daspetemail@gmail.com>
2020-06-17 16:56:38 +0200

    env test

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import 'dotenv/config'
 import express from 'express'
 
 express().get('/', (req, res, next) => {
-    res.json({ hello: 'world 2' })
+    res.json({ hello: process.env.HELLO })
 }).listen(process.env.PORT, '0.0.0.0')
